262fd426a94160925afc6f7516ede22f45fce207
cup0ra <cup0ra@users.noreply.github.com>
2020-03-17 18:44:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   
+    let result = []
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        result.push(expr.slice((i), i + 10))
+    }
+    let b = result.map(s => s.replace(/^0+/, '').replace(/11/g, "-").replace(/10/g, "."));
+    let c =b.map(function(el){
+        if(el === '**********'){
+            return ' ';
+        }else {
+           return el.replace(el, MORSE_TABLE[el]);
+        }
+        
+    });
+    
+    return c.join('');
+    
 }
 
 module.exports = {
